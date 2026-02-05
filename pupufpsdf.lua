22d60f3736268bbe23fea3039da5b36c21a90c83
anowerrrr333-star <anowerrrr333@gmail.com>
2026-02-05 14:14:26 +0500

    Create pupufpsdf.lua

---
+++ pupufpsdf.lua
@@ -0,0 +1,8 @@
+-- by Silent :)
+pcall(function()
+loadstring(game:HttpGet("https://vss.pandadevelopment.net/virtual/file/8c6ed03206c4475b"))()
+end)
+
+pcall(function()
+loadstring(game:HttpGet("https://vss.pandadevelopment.net/virtual/file/d45f7c3471924d56"))()
+end)
