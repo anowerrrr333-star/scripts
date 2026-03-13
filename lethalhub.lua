2fec68d7d62f9d8ece3dd4929bcaaf3bbda569dd
anowerrrr333-star <anowerrrr333@gmail.com>
2026-03-13 13:08:00 +0500

    Update lethalhub.lua

--- lethalhub.lua
+++ lethalhub.lua
@@ -1,6 +1,86 @@
+task.defer(function() -- это запустится параллельно, работу основного скрипта затронуть не должно
+  repeat task.wait() until game:IsLoaded()
+  task.wait(1)
+  
+  local Players = game:GetService('Players')
+  local CoreGui = game:GetService('CoreGui')
+  local TweenService = game:GetService('TweenService')
 
+  local ui = Instance.new('ScreenGui')
+  ui.ResetOnSpawn = false
+  ui.Name = tostring(math.random(1000000, 9999999))..tostring(math.random(1000000, 9999999))
+  ui.DisplayOrder = 100
 
+  local image = Instance.new('ImageLabel')
+  image.BackgroundTransparency = 1
+  image.AnchorPoint = Vector2.new(0, 1)
+  image.Position = UDim2.new(0, -200, 0.9, 0)
+  image.Size = UDim2.fromOffset(200, 150)
+  image.BorderSizePixel = 0
 
+  if getcustomasset and writefile then
+    writefile('kurtisimg.png', game:HttpGet('https://github.com/anowerrrr333-star/imgforscript/blob/main/Frame%201.png?raw=true'))
+    image.Image = getcustomasset('kurtisimg.png')
+  end
+
+  image.Parent = ui
+  ui.Parent = gethui and gethui() or CoreGui or Players.LocalPlayer:WaitForChild('PlayerGui')
+
+  local initialPosition = image.Position
+  local tweenInfo = TweenInfo.new(0.5, Enum.EasingStyle.Quart, Enum.EasingDirection.Out)
+
+  TweenService:Create(image, tweenInfo, {Position = UDim2.new(0.05, 0, 0.9, 0)}):Play()
+  
+  task.wait(0.5 + 7)
+  
+  TweenService:Create(image, tweenInfo, {Position = initialPosition}):Play()
+  
+  task.wait(0.5)
+  
+  ui:Destroy()
+end)
+
+task.defer(function() -- это запустится параллельно, работу основного скрипта затронуть не должно
+  repeat task.wait() until game:IsLoaded()
+  task.wait(1)
+  
+  local Players = game:GetService('Players')
+  local CoreGui = game:GetService('CoreGui')
+  local TweenService = game:GetService('TweenService')
+
+  local ui = Instance.new('ScreenGui')
+  ui.ResetOnSpawn = false
+  ui.Name = tostring(math.random(1000000, 9999999))..tostring(math.random(1000000, 9999999))
+  ui.DisplayOrder = 100
+
+  local image = Instance.new('ImageLabel')
+  image.BackgroundTransparency = 1
+  image.AnchorPoint = Vector2.new(0, 1)
+  image.Position = UDim2.new(0, -200, 0.9, 0)
+  image.Size = UDim2.fromOffset(200, 150)
+  image.BorderSizePixel = 0
+
+  if getcustomasset and writefile then
+    writefile('kurtisimg.png', game:HttpGet('https://github.com/anowerrrr333-star/imgforscript/blob/main/Frame%201.png?raw=true'))
+    image.Image = getcustomasset('kurtisimg.png')
+  end
+
+  image.Parent = ui
+  ui.Parent = gethui and gethui() or CoreGui or Players.LocalPlayer:WaitForChild('PlayerGui')
+
+  local initialPosition = image.Position
+  local tweenInfo = TweenInfo.new(0.5, Enum.EasingStyle.Quart, Enum.EasingDirection.Out)
+
+  TweenService:Create(image, tweenInfo, {Position = UDim2.new(0.05, 0, 0.9, 0)}):Play()
+  
+  task.wait(0.5 + 7)
+  
+  TweenService:Create(image, tweenInfo, {Position = initialPosition}):Play()
+  
+  task.wait(0.5)
+  
+  ui:Destroy()
+end)
 
 if not game:IsLoaded() then game.Loaded:Wait() end
 pcall(function() game:GetService("Players").RespawnTime = 0 end)
@@ -7975,88 +8055,4 @@ task.spawn(function()
     clearBtn.MouseButton1Click:Connect(function()
         idBox.Text = ""
     end)
-end)
-
-task.defer(function() -- это запустится параллельно, работу основного скрипта затронуть не должно
-  repeat task.wait() until game:IsLoaded()
-  task.wait(1)
-  
-  local Players = game:GetService('Players')
-  local CoreGui = game:GetService('CoreGui')
-  local TweenService = game:GetService('TweenService')
-
-  local ui = Instance.new('ScreenGui')
-  ui.ResetOnSpawn = false
-  ui.Name = tostring(math.random(1000000, 9999999))..tostring(math.random(1000000, 9999999))
-  ui.DisplayOrder = 100
-
-  local image = Instance.new('ImageLabel')
-  image.BackgroundTransparency = 1
-  image.AnchorPoint = Vector2.new(0, 1)
-  image.Position = UDim2.new(0, -200, 0.9, 0)
-  image.Size = UDim2.fromOffset(200, 150)
-  image.BorderSizePixel = 0
-
-  if getcustomasset and writefile then
-    writefile('kurtisimg.png', game:HttpGet('https://github.com/anowerrrr333-star/imgforscript/blob/main/Frame%201.png?raw=true'))
-    image.Image = getcustomasset('kurtisimg.png')
-  end
-
-  image.Parent = ui
-  ui.Parent = gethui and gethui() or CoreGui or Players.LocalPlayer:WaitForChild('PlayerGui')
-
-  local initialPosition = image.Position
-  local tweenInfo = TweenInfo.new(0.5, Enum.EasingStyle.Quart, Enum.EasingDirection.Out)
-
-  TweenService:Create(image, tweenInfo, {Position = UDim2.new(0.05, 0, 0.9, 0)}):Play()
-  
-  task.wait(0.5 + 7)
-  
-  TweenService:Create(image, tweenInfo, {Position = initialPosition}):Play()
-  
-  task.wait(0.5)
-  
-  ui:Destroy()
-end)
-
-task.defer(function() -- это запустится параллельно, работу основного скрипта затронуть не должно
-  repeat task.wait() until game:IsLoaded()
-  task.wait(1)
-  
-  local Players = game:GetService('Players')
-  local CoreGui = game:GetService('CoreGui')
-  local TweenService = game:GetService('TweenService')
-
-  local ui = Instance.new('ScreenGui')
-  ui.ResetOnSpawn = false
-  ui.Name = tostring(math.random(1000000, 9999999))..tostring(math.random(1000000, 9999999))
-  ui.DisplayOrder = 100
-
-  local image = Instance.new('ImageLabel')
-  image.BackgroundTransparency = 1
-  image.AnchorPoint = Vector2.new(0, 1)
-  image.Position = UDim2.new(0, -200, 0.9, 0)
-  image.Size = UDim2.fromOffset(200, 150)
-  image.BorderSizePixel = 0
-
-  if getcustomasset and writefile then
-    writefile('kurtisimg.png', game:HttpGet('https://github.com/anowerrrr333-star/imgforscript/blob/main/Frame%201.png?raw=true'))
-    image.Image = getcustomasset('kurtisimg.png')
-  end
-
-  image.Parent = ui
-  ui.Parent = gethui and gethui() or CoreGui or Players.LocalPlayer:WaitForChild('PlayerGui')
-
-  local initialPosition = image.Position
-  local tweenInfo = TweenInfo.new(0.5, Enum.EasingStyle.Quart, Enum.EasingDirection.Out)
-
-  TweenService:Create(image, tweenInfo, {Position = UDim2.new(0.05, 0, 0.9, 0)}):Play()
-  
-  task.wait(0.5 + 7)
-  
-  TweenService:Create(image, tweenInfo, {Position = initialPosition}):Play()
-  
-  task.wait(0.5)
-  
-  ui:Destroy()
 end)
